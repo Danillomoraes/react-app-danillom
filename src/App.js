50fdd78490e490b66d2849c8c1b8aa67bc2577b3
Danillo Moraes <danillo.moraes@outlook.com>
2018-09-11 22:45:24 -0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import { Nav, NavItem, Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+var navbar =
+<Navbar>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <img src="https://image.flaticon.com/icons/svg/174/174862.svg"></img>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    <NavItem>
+      AlgumaCoisa
+    </NavItem>
+    <NavItem>
+      OutraCoisa
+    </NavItem>
+  </Nav>
+</Navbar>;
+
+class navbarClass extends React.Component {
+  render () {
+    return React.createElement (
+      'NavBar',
+      null,
+      React.createElement(
+        'Navbar.Header',
+        null,
+        React.createElement(
+          'Navbar.Brand',
+          null,
+          React.createElement(
+            'img',
+            ['src',"https://image.flaticon.com/icons/svg/174/174862.svg"],
+            null
+          )
+        )
+      )
+    )
+  }
+}
+
+
+
+var element =
+<div>
+  <p>
+    teste bootstrap
+  </p>
+  <Button>euSouUmBotao</Button>
+</div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <navbarClass></navbarClass>
     );
   }
 }
